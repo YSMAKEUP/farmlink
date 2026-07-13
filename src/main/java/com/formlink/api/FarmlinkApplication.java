c8810390a152367c2f543d9e9c3d0f1c6409a14a
Yunki_Song <rldbs111466@gmail.com>
2026-07-13 11:36:19 +0900

    Initial commit: FarmLink Spring Boot 프로젝트 세팅

---
+++ src/main/java/com/formlink/api/FarmlinkApplication.java
@@ -0,0 +1,13 @@
+package com.formlink.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmlinkApplication.class, args);
+	}
+
+}
